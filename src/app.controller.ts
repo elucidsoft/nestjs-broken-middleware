f8a3fae0102d7621a908577d96ae0b1e04e82a76
eric <eric@cloudlayer.io>
2022-10-09 20:39:38 -0400

    push changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
+
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+
+  //POST to this endpoint with an empty content-type and it works and shows the close in the log
+  //POST to it with content-type: application/json and it works but never emits the close event
+  @Post('test')
+  async working(@Req() req) {
+    console.log('Inside test');
+
+    return this.test(req);
+  }
+
+  private async test(req) {
+    //This should emit, but it does not because of the json-body-middleware, why???
+    req.on('close', () => console.log('-- Connection Closed --'));
+
+    //Terminate the connection
+    await new Promise((resolve) => setTimeout(resolve, 60000));
+
     return this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ShutdownSignal } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+
+  });
+
+  app.enableShutdownHooks([ShutdownSignal.SIGINT, ShutdownSignal.SIGQUIT, ShutdownSignal.SIGTERM]);
+
+  await app.listen(3020);
 }
-bootstrap();
+bootstrap().catch((err) => console.error(err));
